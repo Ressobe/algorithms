a7308baadfbc4676c11106d96bd1bb5cdbd06239
Bartosz Sobina <bartosz.sobina@tutanota.com>
2022-02-06 11:42:00 +0100

    Add merge sort

---
+++ merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(array: list) -> list:
+    if len(array) <= 1:
+        return
+    
+    left_array = array[:len(array) // 2]
+    right_array = array[len(array) // 2:]
+    
+    merge_sort(left_array)
+    merge_sort(right_array)
+    
+    i, j, k = 0, 0, 0
+    while i < len(left_array) and j < len(right_array):
+        if left_array[i] < right_array[j]:
+            array[k] = left_array[i]
+            i += 1
+        else:
+            array[k] = right_array[j]
+            j += 1
+        k += 1
+        
+    while i < len(left_array):
+        array[k] = left_array[i]
+        i += 1
+        k += 1
+                
+    while j < len(right_array):
+        array[k] = right_array[j]
+        j += 1
+        k += 1
+        
+    return array
+
+            
+print(merge_sort([0, 2, 1, 8, 3, 4, 22, 90, 4, 1, -20, 45, 82]))
