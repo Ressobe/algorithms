e7b8cc7e3fb40209033d885d05aabf86505f081b
Bartosz Sobina <bartosz.sobina@tutanota.com>
2022-02-06 10:30:33 +0100

    Fix while condition

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,7 +3,7 @@ def insertion_sort(array: list) -> list:
     for i in range(1, length_array):
         current = array[i]
         j = i - 1
-        while j >= 0 and array[j] > current:
+        while j >= 0 and current < array[j]:
             array[j + 1] = array[j]
             j -= 1
             
@@ -11,5 +11,5 @@ def insertion_sort(array: list) -> list:
             
     return array
 
-print(insertion_sort([3, 2, 7, 1, 6]))
-#print(insertion_sort(([0, 2, 1, 8, 3, 4, 22, 90, 4, 1, -20, 45, 82])))
+
+print(insertion_sort(([0, 2, 1, 8, 3, 4, 22, 90, 4, 1, -20, 45, 82])))
