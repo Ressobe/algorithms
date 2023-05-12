44710b44b3b9b26ab87084ff278b2ecdb8c5fb8b
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-05-12 12:19:43 +0200

    Add second version for school

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -31,4 +31,57 @@ def merge_sort(array: list) -> list:
     return array
 
 
-print(merge_sort([0, 2, 1, 8, 3, 4, 22, 90, 4, 1, -20, 45, 82]))
+def SCALAJ(A, p, q, r):
+    n1 = q - p + 1  # Długość lewej podtablicy
+    n2 = r - q  # Długość prawej podtablicy
+
+    # L = A[p:p+n1]  # Skopiuj elementy lewej podtablicy do tablicy L
+    # R = A[q+1:q+1+n2]  # Skopiuj elementy prawej podtablicy do tablicy R
+
+    L = []
+    R = []
+
+    for i in range(n1):
+        L.append(A[p+i])
+
+    for j in range(n2):
+        R.append(A[q+j+1])
+
+    i = j = 0  # Indeksy bieżących elementów w tablicach L i R
+    k = p  # Indeks bieżącego elementu w tablicy A
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:  # Wybierz mniejszy element spośród L[i] i R[j]
+            A[k] = L[i]  # Wstaw mniejszy element do tablicy A
+            i += 1
+        else:
+            A[k] = R[j]  # Wstaw mniejszy element do tablicy A
+            j += 1
+        k += 1
+
+    while i < n1:
+        A[k] = L[i]  # Wstaw pozostałe elementy z tablicy L do tablicy A
+        i += 1
+        k += 1
+
+    while j < n2:
+        A[k] = R[j]  # Wstaw pozostałe elementy z tablicy R do tablicy A
+        j += 1
+        k += 1
+
+
+def SORT_SCALANIE(A, p, r):
+    if p < r:
+        q = (p + r) // 2
+        SORT_SCALANIE(A, p, q)
+        SORT_SCALANIE(A, q + 1, r)
+        SCALAJ(A, p, q, r)
+
+
+def main():
+    array = [0, 2, 1, 8, 3, 4, 22, 90, 4, 1, -20, 45, 82]
+    print(merge_sort(array))
+
+
+if __name__ == "__main__":
+    main()
